7d0049ae86fb7fdab56ec28c7c3b7df3332ba148
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-03-06 19:16:32 -0800

    Add files via upload

---
+++ LongestWord.js
@@ -0,0 +1,10 @@
+function LongestWord(sen) { 
+
+   var words = sen.replace(/[^0-9A-Za-z\s]/g, "").split(" ");
+  var longestWord = words.reduce(function(longestSoFar, currentWord ) {
+    return currentWord.length > longestSoFar.length ? currentWord : longestSoFar;
+  });
+  return longestWord;    
+  
+
+}
